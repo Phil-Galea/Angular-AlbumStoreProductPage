c4fd583b5f495475e88da8867ee03c26ff47f46b
Phil Galea <ph.galea@laposte.net>
2020-11-24 16:46:20 +0100

    Module-3: Try-2

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,21 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import { mergeMap } from 'rxjs/operators';
-import { map } from 'rxjs/operator/map';
-import { Album } from './product-description/album';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
-  private _http: Http;
   private _albumUrl: string = '../assets/album.json';
 
-  constructor() { }
+  constructor(private _http: Http) { }
 
-  getAlbum(id: number): Album {
-    //return this._http.get(this._albumUrl).forEach();
-    return this._http.get(this._albumUrl).subscribe(response => response.json);
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 
 }
